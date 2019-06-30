f0987498638dc85ec3105ef4dfb2f7a45b57384d
Rahul Tiwari <rtiwari@tavisca.in>
2019-06-30 12:59:40 +0530

    Written Test cases & Implement roll ,Score

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -3,12 +3,70 @@ using Xunit;
 
 namespace BowlingBall.Tests
 {
-    public class GameFixture
+    public class GameFixture : IDisposable
     {
-        [Fact]
-        public void DummyTest()
+        private Game game;
+        public  GameFixture()
         {
-            // This is a dummy test that will always pass.
+            game = new Game();
+        }
+        public void Dispose()
+        {
+            game = null;
+        }
+
+        [Fact]
+        public void TestRoll()
+        {
+            game.Roll(10);
+            RollTimes(1, 20);
+        }
+        [Fact]
+        public void TestGetScore()
+        {
+            game.Roll(2);
+            game.Roll(3);
+            RollTimes(1,19);
+            Assert.Equal(23, game.GetScore());
+
+        }
+        [Fact]
+        public void TestSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+            RollTimes(0, 17);
+            Assert.Equal(16, game.GetScore());
+
+        }
+        [Fact]
+        public void TestStrike()
+        {
+            game.Roll(10);
+            game.Roll(5);
+            game.Roll(4);
+            RollTimes(0, 16);
+            Assert.Equal(28, game.GetScore());
+        }
+        [Fact]
+        public void TestAll()
+        {
+            int[] framepins = {10,10,4,5,9,1,5,5,0,0,
+                               1,1,2,2,3,3,10,1,1};
+            for(int i = 0; i < framepins.Length; i++)
+            {
+                game.Roll(framepins[i]);
+            }
+            Assert.Equal(101, game.GetScore());
+        }
+
+        private void RollTimes(int pins,int rolls)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                game.Roll(pins);
+            }
         }
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,16 +4,71 @@ namespace BowlingBall
 {
     public class Game
     {
+        private int[] Rolls = new int[21];
+        private int Index = 0;
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            if (Index < 21)
+            {
+                Rolls[Index++] = pins;
+            } 
+           // throw new NotImplementedException();
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int i = 0;
+            for(int frames = 0; frames < 10; frames++)
+            {
+                if(IsStrike(i))
+                {
+                    score = StrikeScore(score, i);
+                    i += 1;
+                }
+                else if(IsSpare(i))
+                {
+                    score = SpareScore(score, i);
+                    i += 2;
+                }
+                else
+                {
+                    score = Score(score, i);
+                    i += 2;
+                }
+            }
+
+            return score;
+           // throw new NotImplementedException();
         }
 
+        private int Score(int score, int i)
+        {
+            score += Rolls[i] + Rolls[i + 1];
+            return score;
+        }
+
+        private int SpareScore(int score, int i)
+        {
+            score += Rolls[i] + Rolls[i + 1] + Rolls[i + 2];
+            return score;
+        }
+
+        private int StrikeScore(int score, int i)
+        {
+            score += Rolls[i] + Rolls[i + 1] + Rolls[i + 2];
+            return score;
+        }
+
+        private bool IsSpare(int i)
+        {
+            return Rolls[i] + Rolls[i + 1] == 10;
+        }
+
+        private bool IsStrike(int i)
+        {
+            return Rolls[i] == 10;
+        }
     }
 }
 
